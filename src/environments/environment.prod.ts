36a9058a4c6b0823dc35cbe32ae9889e4fda472c
NITHTHA08 <niththav2001@gmail.com>
2024-11-13 02:10:10 -0800

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Handy Service Full App Flutter
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2024-present initappz.
+*/
+export const environment = {
+  production: true,
+  baseUrl: 'http://127.0.0.1:8000/api/',
+  imageUrl: 'http://127.0.0.1:8000/api/v1/storage/images/',
+};
+   
